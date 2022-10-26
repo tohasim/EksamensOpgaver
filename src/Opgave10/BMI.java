94758e0370b44c7d24a2fb96fa779a56fa059354
Tohasim <toreh.simonsen@live.dk>
2022-10-26 15:28:46 +0200

    opgave 10, 11 og 12 lavet

---
+++ src/Opgave10/BMI.java
@@ -0,0 +1,51 @@
+package Opgave10;
+
+import java.util.ArrayList;
+
+public class BMI {
+    private double height;
+    private int weight;
+    private String name;
+
+    public BMI(String name, double height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double calculate(){
+        double BMI = (double) weight/(height * height);
+        return BMI;
+    }
+
+    public boolean isUnderWeight(){
+        return calculate() < 18.5;
+    }
+    public boolean isOverWeight(){
+        return calculate() > 25;
+    }
+
+    public boolean isNormalWeight(){
+        return !isUnderWeight() && !isOverWeight();
+    }
+    public String getName(){
+        return name;
+    }
+
+    public static void main(String[] args) {
+        BMI bmiUnder = new BMI("BmiUnder",1.85, 50);
+        BMI bmiNormal = new BMI("BmiNormal",1.85, 80);
+        BMI bmiOver = new BMI("BmiOver",1.85, 180);
+        ArrayList<BMI> bmis = new ArrayList<>();
+        bmis.add(bmiUnder);
+        bmis.add(bmiNormal);
+        bmis.add(bmiOver);
+        for (BMI bmi: bmis) {
+            System.out.printf("%s: \n" +
+                    "   is underweight: %s\n" +
+                    "   is normalweight: %s\n" +
+                    "   is overweight: %s\n",
+                    bmi.getName(), bmi.isUnderWeight(), bmi.isNormalWeight(), bmi.isOverWeight());
+        }
+    }
+}
---
+++ src/Opgave12/User.java
@@ -0,0 +1,39 @@
+package Opgave12;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class User {
+    private String fullName;
+    private String userID;
+
+    public User(String fullName, String userID) {
+        this.fullName = fullName;
+        createUserID();
+    }
+
+    public boolean validUserID(){
+         return userID.matches("[a-å]{4}[0-9]{4}");
+    }
+    public void createUserID(){
+        Random random = new Random();
+        String firstName;
+        String lastName;
+        String generatedUserID = "";
+        String[] nameArray = fullName.split(" ");
+        firstName = nameArray[0];
+        if (nameArray.length == 2)
+            lastName = nameArray[1];
+        else lastName = nameArray[2];
+        generatedUserID += firstName.substring(0, 2);
+        generatedUserID += lastName.substring(0, 2);
+        generatedUserID += String.format("%04d", random.nextInt(0, 10000));
+        userID = generatedUserID.toLowerCase();
+        System.out.println("USERID: " + userID);
+    }
+
+    public static void main(String[] args) {
+        User testUser = new User("Tore Hald Simonsen", "tosi1234");
+        System.out.println(testUser.validUserID());
+    }
+}
